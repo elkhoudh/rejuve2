2747e3f67cad4d19cad986e7de5dddce68590575
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-06-14 12:44:37 -0400

    Testing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 40){
-            $('.navbar').addClass("fixed-top");
-            $('.navbar .navbar-brand img').attr('src','images/logo-theme.svg');
-            $('.navbar .navbar-toggler img').attr('src','images/icons/menu-theme.png');
-        }
-        else {
-            $('.navbar').removeClass("fixed-top");
-            $('.navbar .navbar-brand img').attr('src', 'images/logo.png');
-            $('.navbar .navbar-toggler img').attr('src', 'images/icons/menu.svg');
-        }
-    });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 40) {
+      $(".navbar").addClass("fixed-top");
+      $(".navbar .navbar-brand img").attr("src", "images/logo-theme.svg");
+      $(".navbar .navbar-toggler img").attr(
+        "src",
+        "images/icons/menu-theme.png"
+      );
+    } else {
+      $(".navbar").removeClass("fixed-top");
+      $(".navbar .navbar-brand img").attr("src", "images/logo.png");
+      $(".navbar .navbar-toggler img").attr("src", "images/icons/menu.svg");
+    }
+  });
 });
